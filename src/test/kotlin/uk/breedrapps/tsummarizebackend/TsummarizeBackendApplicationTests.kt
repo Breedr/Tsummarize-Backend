c28e73978b672fdba8c5b07ff7aae52170875674
Ed George <ed.george@himumsaiddad.com>
2018-03-26 00:45:07 +0100

    Add basic unit tests

--- src/test/kotlin/uk/breedrapps/tsummarizebackend/TsummarizeBackendApplicationTests.kt
+++ src/test/kotlin/uk/breedrapps/tsummarizebackend/TsummarizeBackendApplicationTests.kt
@@ -1,16 +1,70 @@
 package uk.breedrapps.tsummarizebackend
 
+import org.hamcrest.Matcher
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
+import org.mockito.BDDMockito.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.ResponseEntity
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import uk.breedrapps.tsummarizebackend.controller.TsumController
+import uk.breedrapps.tsummarizebackend.model.Tsum
+import java.util.Collections.singletonList
+
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@WebMvcTest(TsumController::class)
 class TsummarizeBackendApplicationTests {
 
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @MockBean
+    lateinit var tsumController: TsumController
+
+    lateinit var tsum: Tsum
+
+    @Before
+    fun before() {
+        tsum = Tsum(
+                1,
+                "Test Tsum",
+                "Test Description",
+                "http://example.com/image.jpg"
+        )
+    }
+
     @Test
-    fun contextLoads() {
+    fun `load tsum list endpoint` () {
+
+        val tsumList = singletonList(tsum)
+
+        given(tsumController.getAllTsums()).willReturn(tsumList)
+
+        mockMvc.perform(get("/api/tsum"))
+                .andExpect(status().isOk)
+    }
+
+    @Test
+    fun `load tsum by id endpoint` () {
+
+        given(tsumController.getTsumById(tsum.id)).willReturn(ResponseEntity.ok(tsum))
+
+        mockMvc.perform(get("/api/tsum/{id}", tsum.id))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("id", isLong(tsum.id)))
+
+    }
+
+    private fun isLong(value: Long): Matcher<Int> {
+        return org.hamcrest.core.Is.`is`(value.toInt())
     }
 
 }
